4434c86885d8e7885a051bd7ebebedecb755ae13
Anderson <andersoninfonet@gmail.com>
2020-09-15 22:55:20 -0300

    🚩GET records

--- backend/src/main/java/com/gmail/andersoninfonet/dspesquisa/controllers/RecordController.java
+++ backend/src/main/java/com/gmail/andersoninfonet/dspesquisa/controllers/RecordController.java
@@ -1,15 +1,22 @@
 package com.gmail.andersoninfonet.dspesquisa.controllers;
 
+import java.time.Instant;
+
 import com.gmail.andersoninfonet.dspesquisa.dto.RecordDTO;
 import com.gmail.andersoninfonet.dspesquisa.dto.RecordInsertDTO;
 import com.gmail.andersoninfonet.dspesquisa.services.RecordService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +29,23 @@ public class RecordController {
     @PostMapping
     public ResponseEntity<RecordDTO> insert(@RequestBody RecordInsertDTO dto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(recordService.insert(dto));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<RecordDTO>> findAll(
+        @RequestParam(value = "min", defaultValue = "") String min,
+        @RequestParam(value = "max", defaultValue = "") String max,
+        @RequestParam(value = "page", defaultValue = "0") Integer page,
+        @RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+        @RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+        @RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+
+        Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+        Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+        linesPerPage = Integer.valueOf(0).equals(linesPerPage) ? Integer.MAX_VALUE : linesPerPage; 
+
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+        return ResponseEntity.ok().body(recordService.findByMoment(minDate, maxDate, pageRequest));
     }
 }
--- backend/src/main/java/com/gmail/andersoninfonet/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/gmail/andersoninfonet/dspesquisa/repositories/RecordRepository.java
@@ -1,11 +1,21 @@
 package com.gmail.andersoninfonet.dspesquisa.repositories;
 
+import java.time.Instant;
+
 import com.gmail.andersoninfonet.dspesquisa.entities.Record;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
+
+    @Query("SELECT obj FROM Record obj WHERE " + 
+            "(coalesce(:minDate, null) IS NULL OR obj.moment >= :minDate) AND " + 
+            "(coalesce(:maxDate, null) IS NULL OR obj.moment <= :maxDate)")
+	Page<Record> findByMoment(Instant minDate, Instant maxDate, Pageable pageable);
     
 }
